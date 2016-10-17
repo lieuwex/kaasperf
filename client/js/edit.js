16e6dc8ba66e6d96cf087192cd79e7f6cac2fc39
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-10-17 20:46:18 +0200

    handjeswapper3

--- client/js/edit.js
+++ client/js/edit.js
@@ -1,3 +1,4 @@
+var $form = document.getElementsByTagName('form')[0];
 var $title = document.getElementById('title');
 var $description = document.getElementById('description');
 var $submit = document.getElementById('submit');
@@ -6,9 +7,14 @@ var create = document.querySelector('meta[key="create-mode"]').getAttribute('val
 var publicId = document.querySelector('meta[key="publicId"]').getAttribute('value');
 var privateId = document.querySelector('meta[key="privateId"]').getAttribute('value');
 
+$form.addEventListener('submit', function (event) {
+	event.preventDetault();
+});
+
 $submit.addEventListener('click', function (event) {
 	event.preventDetault();
 	fetch(create ? `/create` : `/${publicId}/edit/${privateId}`, {
 		method: 'post',
+		body: new FormData($form),
 	}).then(r => console.log(r));
 });
